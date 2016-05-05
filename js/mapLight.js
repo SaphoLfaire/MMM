a5e3a9723a603f9ebe25973f60f65b1c28cbc240
fredddjs <frederic.bertolo@gmail.com>
2016-05-05 12:54:19 +0200

    Update mapLight.js

--- js/mapLight.js
+++ js/mapLight.js
@@ -492,8 +492,8 @@ function change() {
 
     var button_id = this.id;
     console.log("tu viens de cliquer sur le bouton de "+ button_id);
-    if (document.getElementById(button_id).style.fill !== "#FF0000"){
-	document.getElementById(button_id).style.fill = "#FF0000";
+    if (document.getElementById(button_id).style.fill !== "Red"){
+	document.getElementById(button_id).style.fill = "Red";
 	g.selectAll(".internshipFr")
 	    .filter(function(d){return d.properties.year == button_id;})
 	    .transition()
@@ -509,8 +509,8 @@ function change() {
 	return null;
 	
     }
-    if (document.getElementById(button_id).style.fill !== "#008000"){
-	document.getElementById(button_id).style.fill = "#008000";
+    if (document.getElementById(button_id).style.fill !== "Green"){
+	document.getElementById(button_id).style.fill = "Green";
 	g.selectAll(".internshipFr")
 	    .filter(function(d){return d.properties.year == button_id;})
 	    .transition()
